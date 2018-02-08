59e541170a5e2a3fe2c1c8f3fe58b6b182c60602
EvaFro <frolli.erin@gmail.com>
2018-02-08 12:31:42 -0800

    Started creating our worker fucntion

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,11 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var path = require('path');
+var _ = require('underscore');
+var http = require('http');
+
+
+exports = archive.readListOfUrls(urls=>console.log("the file contians these ",urls));
+
