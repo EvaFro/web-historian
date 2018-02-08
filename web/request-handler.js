748d66cb738a1ad3794bb4664172734672c79262
EvaFro <frolli.erin@gmail.com>
2018-02-08 12:30:59 -0800

    Added the res.code to handel differnt res cases

--- web/request-handler.js
+++ web/request-handler.js
@@ -9,9 +9,16 @@ exports.handleRequest = function (req, res) {
   const reqType = req.method;
   
   if (reqType === 'GET') {
+    res.writeHead(200, http.headers);
     if (req.url === '/') {
       let newUrl = `${archive.paths.siteAssets}/index.html`;
       http.serveAssets(res, newUrl);
+
+    } else if (req.url === '/styles.css') {
+      let testUrl = `${archive.paths.siteAssets}${req.url}`;
+      http.serveAssets(res, testUrl, (data) => {
+        res.end(data);
+      });
     } else {
       let testUrl = `${archive.paths.archivedSites}/${req.url}`;
       http.serveAssets(res, testUrl, (data) => {
@@ -23,12 +30,19 @@ exports.handleRequest = function (req, res) {
   if (reqType === 'POST') {
     req.on('data', (data) => {
       let workingUrl = data.toString().slice(4);
-      res.writeHead(302);
+      res.writeHead(302, http.headers);
       archive.isUrlInList(workingUrl, (exsits)=>{
         if (exsits) {
-          var asset = `${archive.paths.archivedSites}/${workingUrl}`;
-          console.log("asset is ", asset);
-          http.serveAssets(res, asset);
+          archive.isUrlArchived(workingUrl, (exsits) => {
+            if (exsits) {
+              var asset = `${archive.paths.archivedSites}/${workingUrl}`;
+              http.serveAssets(res, asset);
+            } else {
+              archive.downloadUrls([workingUrl]);
+              var asset = `${archive.paths.siteAssets}/loading.html`;
+              http.serveAssets(res, asset);
+            }
+          });
         } else {
           archive.addUrlToList(workingUrl, () => {
             console.log('done!');
@@ -40,6 +54,5 @@ exports.handleRequest = function (req, res) {
         }
       });
     });
-    res.end(data); /// check if im right!
   }
 };
