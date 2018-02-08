765a19cd60af5089e8bede4336a4cea4c3202035
EvaFro <frolli.erin@gmail.com>
2018-02-07 18:06:52 -0800

    Added code 2 render the html pages

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,28 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
+// importing from the httphelper
+const http = require('./http-helpers');
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  const reqType = req.method;
+  
+  if (reqType === 'GET') {
+    if (req.url === '/') {
+      let newUrl = `${archive.paths.siteAssets}/index.html`;
+      
+      
+      http.serveAssets(res, newUrl);
+    } else {
+      let testUrl = `${archive.paths.archivedSites}${req.url}`;
+      http.serveAssets(res, testUrl);
+    }
+  }
+  
+  if (reqType === 'POST') {
+    console.log('ReqType is POST, and doing this');
+
+    res.end(archive.paths.list);
+  }
+  
 };
